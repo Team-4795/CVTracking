3994964de6a435c1bf90ef021cc5983668f0e53a
Charles Zhang <charleszhang99@yahoo.com>
2017-02-09 16:30:40 -0500

    Comment out unused algorithms

--- src/CV.cpp
+++ src/CV.cpp
@@ -245,119 +245,118 @@ void findConvexHull(Image_capsule &images, vector< vector<Point> > &contours, ve
       double cx = 360;
       double f = 1078;
       angle = atan((u - cx) / f);
-      angle = angle * 180 / M_PI;
-      printf("target#%d: Area: %d X:%d Y:%d Angle: %f \n", c, area, u, v, angle);
+      printf("target#%d: Area: %d X:%d Y:%d Angle: %f \n", c, area, u, v, radian_to_degrees(angle));
       count++;
     }
   }
-
-}
-void findBoundingBox(Image_capsule &images, vector< vector<Point> > &contours)
-{
-  vector< vector<Point> > contours_poly(contours.size());
-  vector<Rect> boundRect(contours.size());
-  vector<Rect> filteredRect(boundRect.size());
-
-  for (size_t i = 0; i < contours.size(); i++)
-  {
-    approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
-    boundRect[i] = boundingRect(Mat(contours_poly[i]));
-  }
-
-  for (size_t i = 0; i < contours.size(); i++)
-  {
-    Scalar color = Scalar(255, 0, 0);
-    rectangle(images.frame, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0);
-  }
 }
 
+// void findBoundingBox(Image_capsule &images, vector< vector<Point> > &contours)
+// {
+//   vector< vector<Point> > contours_poly(contours.size());
+//   vector<Rect> boundRect(contours.size());
+//   vector<Rect> filteredRect(boundRect.size());
 
-//// currently unused algorithm
-Arectangle rects[10];
-Arectangle finalRects[10];
+//   for (size_t i = 0; i < contours.size(); i++)
+//   {
+//     approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
+//     boundRect[i] = boundingRect(Mat(contours_poly[i]));
+//   }
 
-// helper function:
-// finds a cosine of angle between vectors
-// from pt0->pt1 and from pt0->pt2
-static double vector_cos(Point pt1, Point pt2, Point pt0)
-{
-  double dx1 = pt1.x - pt0.x;
-  double dy1 = pt1.y - pt0.y;
-  double dx2 = pt2.x - pt0.x;
-  double dy2 = pt2.y - pt0.y;
-  return (dx1 * dx2 + dy1 * dy2) / sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2));
-}
+//   for (size_t i = 0; i < contours.size(); i++)
+//   {
+//     Scalar color = Scalar(255, 0, 0);
+//     rectangle(images.frame, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0);
+//   }
+// }
 
-void findSquares(Image_capsule &images, vector< vector<Point> > &contours)
-{
-  int pos_thresh = 3;
 
-  //approximation of sqaures and their properties
-  vector<Point> approx;
-  vector< vector<Point> > squares;
-  // test each contour to see if its a square
-  for (size_t i = 0; i < contours.size(); i++)
-  {
-    approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true) * 0.02, true);
-    if (approx.size() == 4 &&
-        fabs(contourArea(Mat(approx))) > 1000 &&
-        isContourConvex(Mat(approx)))
-    {
-      double maxCosine = 0.3;
+// //// currently unused algorithm
+// Arectangle rects[10];
+// Arectangle finalRects[10];
 
-      for (int j = 2; j < 5; j++)
-      {
-        // find the maximum cosine of the angle between joint edges
-        double cosine = fabs(vector_cos(approx[j % 4], approx[j - 2], approx[j - 1]));
-        maxCosine = MAX(maxCosine, cosine);
-      }
+// // helper function:
+// // finds a cosine of angle between vectors
+// // from pt0->pt1 and from pt0->pt2
+// static double vector_cos(Point pt1, Point pt2, Point pt0)
+// {
+//   double dx1 = pt1.x - pt0.x;
+//   double dy1 = pt1.y - pt0.y;
+//   double dx2 = pt2.x - pt0.x;
+//   double dy2 = pt2.y - pt0.y;
+//   return (dx1 * dx2 + dy1 * dy2) / sqrt((dx1 * dx1 + dy1 * dy1) * (dx2 * dx2 + dy2 * dy2));
+// }
 
-      if (maxCosine < 0.6)
-        squares.push_back(approx);
-    }
-  }
-  //go through the squares to find their size and draw them
-  for (size_t i = 0; i < squares.size(); i++)
-  {
-    const Point *p = &squares[i][0];
-    int n = (int)squares[i].size();
+// void findSquares(Image_capsule &images, vector< vector<Point> > &contours)
+// {
+//   int pos_thresh = 3;
 
-    double cx = (p[2].x + p[0].x) / 2;
-    double cy = (p[2].y + p[0].y) / 2;
-    circle(images.frame, Point(cx, cy), 1, Scalar(0, 0, 0), 3, CV_AA);
+//   //approximation of sqaures and their properties
+//   vector<Point> approx;
+//   vector< vector<Point> > squares;
+//   // test each contour to see if its a square
+//   for (size_t i = 0; i < contours.size(); i++)
+//   {
+//     approxPolyDP(Mat(contours[i]), approx, arcLength(Mat(contours[i]), true) * 0.02, true);
+//     if (approx.size() == 4 &&
+//         fabs(contourArea(Mat(approx))) > 1000 &&
+//         isContourConvex(Mat(approx)))
+//     {
+//       double maxCosine = 0.3;
 
-    double area = abs(p[2].x - p[0].x) * abs(p[2].y - p[0].y);
-    rects[i].cx = cx;
-    rects[i].cy = cy;
-    rects[i].area = area;
-    //printf("Square# %d, Center: X: %d Y: %d, Area: %d\n",(int) i,(int) cx,(int) cy,(int) area);
-    polylines(images.frame, &p, &n, 1, true, Scalar(255, 0, 0), 2, CV_AA);
-  }
+//       for (int j = 2; j < 5; j++)
+//       {
+//         // find the maximum cosine of the angle between joint edges
+//         double cosine = fabs(vector_cos(approx[j % 4], approx[j - 2], approx[j - 1]));
+//         maxCosine = MAX(maxCosine, cosine);
+//       }
 
-  //merge duplicate squares into one average
-  if (squares.size() > 1)
-  {
-    int c = 0;
-    for (size_t i = 0; i < squares.size(); i++)
-    {
-      for (size_t y = i + 1; y < squares.size(); y++)
-      {
-        if (i != y)
-        {
-          if (rects[i].cx < rects[y].cx + pos_thresh || rects[i].cx > rects[y].cx - pos_thresh)
-          {
-            if (rects[i].cy < rects[y].cy + pos_thresh || rects[i].cy > rects[y].cy - pos_thresh)
-            {
-              finalRects[c].cx = (rects[i].cx + rects[y].cx) / 2;
-              finalRects[c].cy = (rects[i].cy + rects[y].cy) / 2;
-              finalRects[c].area = (rects[i].area + rects[y].area) / 2 ;
-              printf("Square# %d, Center: X: %d Y: %d, Area: %d\n"
-                     , (int) i, (int) finalRects[i].cx, (int) finalRects[i].cy, (int) finalRects[i].area);
-              c++;
-            }
-          }
-        }
-      }
-    }
-  }
-}
+//       if (maxCosine < 0.6)
+//         squares.push_back(approx);
+//     }
+//   }
+//   //go through the squares to find their size and draw them
+//   for (size_t i = 0; i < squares.size(); i++)
+//   {
+//     const Point *p = &squares[i][0];
+//     int n = (int)squares[i].size();
+
+//     double cx = (p[2].x + p[0].x) / 2;
+//     double cy = (p[2].y + p[0].y) / 2;
+//     circle(images.frame, Point(cx, cy), 1, Scalar(0, 0, 0), 3, CV_AA);
+
+//     double area = abs(p[2].x - p[0].x) * abs(p[2].y - p[0].y);
+//     rects[i].cx = cx;
+//     rects[i].cy = cy;
+//     rects[i].area = area;
+//     //printf("Square# %d, Center: X: %d Y: %d, Area: %d\n",(int) i,(int) cx,(int) cy,(int) area);
+//     polylines(images.frame, &p, &n, 1, true, Scalar(255, 0, 0), 2, CV_AA);
+//   }
+
+//   //merge duplicate squares into one average
+//   if (squares.size() > 1)
+//   {
+//     int c = 0;
+//     for (size_t i = 0; i < squares.size(); i++)
+//     {
+//       for (size_t y = i + 1; y < squares.size(); y++)
+//       {
+//         if (i != y)
+//         {
+//           if (rects[i].cx < rects[y].cx + pos_thresh || rects[i].cx > rects[y].cx - pos_thresh)
+//           {
+//             if (rects[i].cy < rects[y].cy + pos_thresh || rects[i].cy > rects[y].cy - pos_thresh)
+//             {
+//               finalRects[c].cx = (rects[i].cx + rects[y].cx) / 2;
+//               finalRects[c].cy = (rects[i].cy + rects[y].cy) / 2;
+//               finalRects[c].area = (rects[i].area + rects[y].area) / 2 ;
+//               printf("Square# %d, Center: X: %d Y: %d, Area: %d\n"
+//                      , (int) i, (int) finalRects[i].cx, (int) finalRects[i].cy, (int) finalRects[i].area);
+//               c++;
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
